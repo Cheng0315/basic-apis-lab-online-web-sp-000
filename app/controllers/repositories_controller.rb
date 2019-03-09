edc3a7c6bc669b141f6eabe568b6a2cc01051aa8
Cheng Vue <csalvue@gmail.com>
2019-03-09 23:06:11 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,6 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = 'Iv1.8b5c60b5e7060cde'
       req.params['client_secret'] = '0acf28d9c3267c7386ec2f5742f470ee598a2f8c'
