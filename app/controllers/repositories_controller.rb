3720b49de9786c2cf79c7fd83c9281fb32b1703f
Cheng Vue <csalvue@gmail.com>
2019-03-09 22:39:27 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,10 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      req.params['client_id'] = Iv1.8b5c60b5e7060cde
+      req.params['client_secret'] = 0acf28d9c3267c7386ec2f5742f470ee598a2f8c
+      req.params['query'] = 'tetris'
+    end
   end
 end
